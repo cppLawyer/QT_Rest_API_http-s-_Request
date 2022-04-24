effea558ac457c734e5d52b255ee0d18f9f90efe
cppLawyer <76749832+cppLawyer@users.noreply.github.com>
2022-04-24 11:50:40 +0200

    Create Rest_Api.cpp

---
+++ Rest_Api.cpp
@@ -0,0 +1,26 @@
+
+void sendRequest(){
+
+    // create custom temporary event loop on stack
+    QEventLoop eventLoop;
+
+    // "quit()" the event-loop, when the network request "finished()"
+    QNetworkAccessManager mgr;
+    QObject::connect(&mgr, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+    // the HTTP request
+    QNetworkRequest req( QUrl( QString("https/http url with json respons or somehting else") ) );
+    QNetworkReply *reply = mgr.get(req);
+    eventLoop.exec(); // blocks stack until "finished()" has been called
+
+    if (reply->error() == QNetworkReply::NoError) {
+        //success
+        qDebug() << "Success" <<reply->readAll();
+        delete reply;
+    }
+    else {
+        //failure
+        qDebug() << "Failure" <<reply->errorString();
+        delete reply;
+    }
+}//just call the function and change return type to std::string so you can reuse info
